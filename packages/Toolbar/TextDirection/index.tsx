421059f90218c62c91d2623e94ac7d80d2209bfe
sunyang <2480901422@qq.com>
2024-05-20 21:51:32 +0800

    feat: text direction

---
+++ packages/Toolbar/TextDirection/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Quill from 'quill'
+import { IconTextDirectionLtr, IconTextDirectionRtl } from '@tabler/icons-react'
+
+type Props = {
+  editor: Quill | null
+}
+
+const TextDirection: React.FC<Props> = ({ editor }) => {
+  const [direction, setDirection] = useState(false)
+  const btnRef = useRef<HTMLButtonElement>(null)
+
+  const handleTextDirection = () => {
+    if (!editor || !btnRef.current) {
+      return
+    }
+
+    const format = editor.getFormat()
+    if (format.hasOwnProperty('direction')) {
+      editor.format('direction', false)
+      editor.format('align', false)
+      setDirection(false)
+    } else {
+      editor.format('direction', 'rtl')
+      editor.format('align', 'right')
+      setDirection(true)
+    }
+  }
+
+  useEffect(() => {
+    if (!btnRef.current) {
+      return
+    }
+    if (btnRef.current) {
+      btnRef.current.addEventListener('click', handleTextDirection)
+    }
+    return () => {
+      btnRef.current?.removeEventListener('click', handleTextDirection)
+    }
+  }, [editor])
+
+  return (
+    <button ref={btnRef}>
+      {direction ? <IconTextDirectionRtl className="rq-icon" /> : <IconTextDirectionLtr className="rq-icon" />}
+    </button>
+  )
+}
+
+export default TextDirection
