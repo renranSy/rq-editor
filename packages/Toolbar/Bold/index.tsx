3fbe640b253289479a3330606078790cf9275954
sunyang <2480901422@qq.com>
2024-05-20 21:54:36 +0800

    feat: bold

--- packages/Toolbar/Bold/index.tsx
+++ packages/Toolbar/Bold/index.tsx
@@ -1,17 +1,84 @@
-import React from 'react'
-import Quill from 'quill'
+import React, { useEffect, useRef } from 'react'
+import Quill, { Range } from 'quill'
 import { IconBold } from '@tabler/icons-react'
 
 type Props = {
-  name: string
-  key: string
-  editorRef: HTMLDivElement
-  editor: Quill
+  editor: Quill | null
 }
 
-export const Bold: React.FC<Props> = (props) => {
+const Bold: React.FC<Props> = ({ editor }) => {
+  const btnRef = useRef<HTMLButtonElement>(null)
+  const setActive = (element: HTMLButtonElement, status: boolean) => {
+    if (status) {
+      element.style.backgroundColor = '#f2f5f9'
+    } else {
+      element.style.backgroundColor = '#ffffff'
+    }
+  }
+
+  const handleBold = () => {
+    if (!editor || !btnRef.current) {
+      return
+    }
+    const selection = editor.getSelection()
+    if (selection && selection.length != 0) {
+      const format = editor.getFormat(selection.index, selection.length)
+      if (format.hasOwnProperty('bold')) {
+        editor.formatText(selection.index, selection.length, 'bold', false)
+        setActive(btnRef.current, false)
+      } else {
+        editor.format('bold', true)
+        setActive(btnRef.current, true)
+      }
+    } else {
+      const format = editor.getFormat()
+      if (format.hasOwnProperty('bold')) {
+        editor.format('bold', false)
+        setActive(btnRef.current, false)
+      } else {
+        editor.format('bold', true)
+        setActive(btnRef.current, true)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (!btnRef.current) {
+      return
+    }
+    if (btnRef.current) {
+      btnRef.current.addEventListener('click', handleBold)
+    }
+    return () => {
+      btnRef.current?.removeEventListener('click', handleBold)
+    }
+  }, [editor])
+
+  useEffect(() => {
+    if (!editor || !btnRef.current) {
+      return
+    }
+
+    const handler = (range: Range) => {
+      if (!range || !btnRef.current) {
+        return
+      }
+      const isFormat =
+        editor.getFormat(range.index, range.length).hasOwnProperty('bold') || editor.getFormat().hasOwnProperty('bold')
+      if (isFormat) {
+        setActive(btnRef.current, true)
+      } else {
+        setActive(btnRef.current, false)
+      }
+    }
+    editor.on('selection-change', handler)
+
+    return () => {
+      editor.off('selection-change', handler)
+    }
+  }, [editor])
   return (
-    <button>
+    <button ref={btnRef}>
       <IconBold className="rq-icon" />
     </button>
   )
