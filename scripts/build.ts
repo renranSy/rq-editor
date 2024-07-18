2a264bcdbbcb8ce316234d1052b366d9d8d9ffb0
sunyang <2480901422@qq.com>
2024-07-18 22:49:36 +0800

    fix: build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import path from 'path'
-import { build, LibraryFormats } from 'vite'
+import { build } from 'vite'
 import removeDir from './utils/removeDir'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
@@ -9,91 +9,53 @@ const __dirname = fileURLToPath(new URL('..', import.meta.url))
 const resolvePath = (p: string) => path.resolve(__dirname, p)
 
 !(async () => {
-  const moduleEntry = {
-    index: resolvePath('packages'),
-    RQEditor: resolvePath('packages/RQEditor')
-  }
-  const formats: LibraryFormats[] = ['es', 'cjs', 'umd']
-
-  const entries = {
-    es: {
-      ...moduleEntry
-    },
-    cjs: moduleEntry,
-    umd: resolvePath('packages')
-  }
-
   removeDir(resolvePath('lib'))
 
-  const externalNames = {
-    es: 'mjs',
-    cjs: 'cjs'
-  }
-
-  await Promise.all(
-    formats.map((t) => {
-      return build({
-        base: '/',
-        plugins: [
-          react(),
-          t === 'es' &&
-            dts({
-              include: [resolvePath('packages')],
-              outDir: resolvePath('lib/types')
-            })
-        ],
-        resolve: {
-          alias: {
-            '~': resolvePath('packages')
-          }
-        },
-        css: {
-          modules: {
-            localsConvention: 'camelCase'
-          },
-          preprocessorOptions: {
-            less: {
-              javascriptEnabled: true
-            }
-          }
-        },
-        build: {
-          emptyOutDir: false,
-          cssCodeSplit: true,
-          outDir: resolvePath('lib'),
-          lib: {
-            entry: entries[t],
-            name: 'RQEditor',
-            formats: [t],
-            fileName(format) {
-              switch (format) {
-                case 'es': {
-                  return 'es/[name].mjs'
-                }
-                case 'cjs':
-                  return 'cjs/[name].cjs'
-                default: {
-                  return 'umd/index.js'
-                }
-              }
-            }
-          },
-          rollupOptions: {
-            external: t === 'umd' ? ['react', 'react-dom'] : ['react', 'react-dom', 'quill'],
-            output: {
-              chunkFileNames: `${t}/chunks/[name].${externalNames[t]}`,
-              assetFileNames: 'index[extname]',
-              globals:
-                t === 'umd'
-                  ? {
-                      react: 'React',
-                      'react-dom': 'ReactDOM'
-                    }
-                  : {}
-            }
+  build({
+    base: '/',
+    plugins: [
+      react(),
+      dts({
+        include: [resolvePath('packages')],
+        outDir: resolvePath('lib'),
+        staticImport: true,
+        rollupTypes: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '~': resolvePath('packages')
+      }
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCase'
+      },
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true
+        }
+      }
+    },
+    build: {
+      emptyOutDir: false,
+      cssCodeSplit: true,
+      outDir: resolvePath('lib'),
+      lib: {
+        entry: resolvePath('packages/index.ts'),
+        name: 'RQEditor',
+        fileName: 'index'
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom', 'quill'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            quill: 'Quill'
           }
         }
-      })
-    })
-  )
+      }
+    }
+  })
 })()
